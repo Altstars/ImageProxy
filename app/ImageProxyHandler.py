2b34c31c8a290a7499baccac01f6d23156b7f878
Kouki Saito <dan.addr.skd@gmail.com>
2014-07-19 19:45:50 +0900

    Show Sample image

--- app/ImageProxyHandler.py
+++ app/ImageProxyHandler.py
@@ -4,5 +4,17 @@ from models.model import *
 
 class ImageProxyHandler(BaseHandler):
   def get(self):
+    url = self.get_argument("url")
+    
+    image = self._get_image(url)
+    self.set_header('Content-type', 'image/png')
+    self.set_header('Content-length', len(image))
+    self.write(image)
+ 
     self.render("index.html")
+  def _get_image(self,url):
+    f = open("./images/sample.png","rb")
+    return f.read()
+
+
 
