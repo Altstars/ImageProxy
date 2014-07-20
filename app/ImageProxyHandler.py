6a1d1bc7902619f6c309cdbb462704f772b24984
Kouki Saito <dan.addr.skd@gmail.com>
2014-07-20 22:53:08 +0900

    Return a image with url

--- app/ImageProxyHandler.py
+++ app/ImageProxyHandler.py
@@ -1,20 +1,27 @@
 
 from app.BaseHandler import BaseHandler
 from models.model import *
+import urllib.request
 
 class ImageProxyHandler(BaseHandler):
   def get(self):
     url = self.get_argument("url")
     
     image = self._get_image(url)
-    self.set_header('Content-type', 'image/png')
-    self.set_header('Content-length', len(image))
-    self.write(image)
+    self.set_header('Content-type', image["content-type"])
+    self.set_header('Content-length', len(image["data"]))
+    self.write(image["data"])
  
     self.render("index.html")
-  def _get_image(self,url):
-    f = open("./images/sample.png","rb")
-    return f.read()
+  def _get_image(self, url):
+    image_url = urllib.request.urlopen(url)
+    content_type = image_url.info().get("content-type")
+    image = image_url.read()
+
+    return {
+        "content-type":content_type,
+        "data":image,
+    }
 
 
 
